75f039f3febaa7f9bf1f8e3492f402d6c06af70d
oscar_felipe <groovenino@gmail.com>
2015-06-05 19:59:13 +0200

    implement add word

--- app/scripts/controllers/word.js
+++ app/scripts/controllers/word.js
@@ -8,16 +8,39 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-    .controller('AddWordController', function ($scope) {
+    .controller('AddWordController', function ($scope,$http,$location) {
+
+
         $scope.add = function(word){
-           $scope.words.push(word);
-            console.log(word);
+           //format my word into a Javascript Object
+            var myWord = {'word': {'title': word.title, 'language':'dk',
+                'definition':word.definition,'examples': word.examples}};
+
+
+            var req = {
+                method: 'PUT',
+                url:'http://localhost:8080/wc/words',
+                headers: {
+
+                    'Content-Type': 'application/json'
+                },
+                useXDomain : true,
+                data: myWord
+            };
+
+            $http(req)
+                .success(
+                    function(response) {
+                        console.log("parece que resultó " + JSON.stringify(response))
+                    });
+
+
             console.log("holi");
-            console.log($scope.words);
+
         };
 
-        $scope.words = [{title:"hund",definition:"perro"},{title:"kat",definition:"gato"},
-            {title:"blondine",definition:"rubia"}];
+
+
     });/**
  * Created by oscar on 03-06-15.
  */
